92500a758e4c724efaecd035e8c919c6112871ce
Erik Nilsson <eriknilsson89@gmail.com>
2012-03-01 13:13:16 +0100

    Code refactoring

--- ToDo/js/collections/tags.js
+++ ToDo/js/collections/tags.js
@@ -4,8 +4,15 @@ define([
 ], function (Backbone, tagModel) {
     tagCollection = Backbone.Collection.extend({
         localStorage: new Store("tags"),
-        model: tagModel
-
+        model: tagModel,
+        //funktion som kollar om tagg-namnet redan finns
+        doesTagnameExist: function (tagName) {
+            for (var i = 0; i < this.length; i++) {
+                if (this.models[i].get("Text").toLowerCase() === tagName.toLowerCase())
+                    return true;
+            }
+            return false;
+        }
     });
     return tagCollection
 });
